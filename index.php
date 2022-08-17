a0e6d4a8bc590efdf4503683d08e26f1b442d043
Michael <oluochmichael87@gmail.com>
2022-08-17 16:03:26 +0300

    Automatic git based commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <!-- Latest compiled and minified CSS -->
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+<!-- Javascript popper-->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<title>Homepage</title>
+</head>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="announce.php">Announcements</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+            <li><a href="adminlogin.php" class="btn btn-primary">Admin Login</a></li>
+            <li><a href="userlogin.php" class="btn btn-success">User Login</a></li>
+            <hr>
+        </ul>
+    </nav>
+</header>
+<body>
+    <div class="section_1">
+        <h1 class="img-text">WELCOME TO OUR CHURCH!</h1>
+        <img class="main_img" src="/images/pexels.JPG">
+    </div>
+
+    <section class="announce">
+        <br>
+        <br>
+        <br>
+        <h2>ANNOUNCEMENTS</h2>
+        <br>
+        <p1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet 
+            voluptatum velit quis odio natus sapiente 
+            ad reiciendis</p1>
+    </section>
+        <br>
+        <br>
+        <br> 
+        <br>
+        <br>
+        <br>
+    <section>
+        <a href="signup.php" class="btn btn-success">Open your Account here</a>
+        <br>
+    <br>
+    <br>
+    <br>
+    </section>
+    
+</body>
+</html>
---
+++ userlogin.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <!-- Latest compiled and minified CSS -->
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+<title>User Login</title>
+</head>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="announce.php">Announcements</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+            <li><a href="adminlogin.php" class="btn btn-primary">Admin Login</a></li>
+            <li><a href="userlogin.php" class="btn btn-success">User Login</a></li>
+            <hr>
+        </ul>
+    </nav>
+</header>
+<body>
+    <center>
+    <form class="login" action="login_check.php" method="POST">
+        <h1> User Login Form </h1> 
+        <h5>
+            <?php
+                error_reporting(0);
+                session_start();
+                session_destroy();
+                echo $_SESSION['loginMessage'];
+            ?>
+        </h5>
+        <div class="form">  
+            <div> 
+            <label>Email : </label>   
+            <input type="email" placeholder="Enter email" name="email" required> 
+            </div>
+            <div> 
+            <label>Password : </label>   
+            <input type="password" placeholder="Enter Password" name="password" required>  
+            </div>
+            <div>
+            <button type="submit">Login</button>     
+            <button type="button" class="cancelbtn"> Cancel</button>   
+            </div>  
+        </div>   
+    </form>     
+    </center>
+</body>
+</html>
