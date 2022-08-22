aa5fe0de3988106dbc8f8226163af6e84b1838a4
Michael <oluochmichael87@gmail.com>
2022-08-22 14:02:39 +0300

    admin portal minor changes, dashboard added.

--- adminportal.php
+++ adminportal.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="dash.css">
     <!-- Latest compiled and minified CSS -->
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
@@ -22,30 +22,41 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <title>Admin Portal</title>
 </head>
-<header>
-    <nav>
-        <ul>
-            <li><a href="adminportal.php">Administrator Home</a></li>
 
-            <a href="logout.php" class="btn btn-danger">Logout</a>
-            <hr>
-        </ul>
-    </nav>
+<header class="header">
+		
+		<a href="adminportal.php">This is the admin dashboard</a>
+		<div class="logout">	
+        <a href="logout.php" class="btn btn-danger">Logout</a>
+        <hr>
+		</div>
+
 </header>
-
 <body>
-    <div class="adminsection_1">
-        <h1 class="img-text">WELCOME, THIS IS THE ADMIN PORTAL!</h1>
-    </div>
-
-
     <aside>
         <ul>
+            <li><a href="adminportal.php">DASHBOARD HOME</a>
             <li><a href="">VIEW EXISTING MEMBERS</a></li>
             <li><a href=""> </a>UPDATE EVENTS</li>
             <li><a href=""> </a>UPDATE CHURCH ANNOUNCEMENTS</li>
         </ul>
     </aside>
 
-    
+    <div class="content">
+
+        <div class="adminsection_1">
+            <h1 class="img-text">WELCOME, THIS IS THE ADMIN PORTAL!</h1>
+        </div>
+		
+		<h1>Sidebar Accordion</h1>
+
+		<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, dicta suscipit earum nostrum 
+            ratione, et rerum placeat iure, consectetur esse sunt quam soluta dolore animi odit quasi 
+            fugiat eligendi iste?</p>
+
+
+
+	</div>
+
+
 </body>
